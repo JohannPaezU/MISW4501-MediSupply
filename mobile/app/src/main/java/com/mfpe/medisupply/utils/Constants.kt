0edb2a21ce76a31248417673817b4af91d38cd27
Miguel Fernando Padilla Espino <m.padillae@uniandes.edu.co>
2025-10-11 09:48:46 -0500

    mobile: update InicioFragment and layout files for user greeting and UI enhancements

--- mobile/app/src/main/java/com/mfpe/medisupply/utils/Constants.kt
+++ mobile/app/src/main/java/com/mfpe/medisupply/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.mfpe.medisupply.utils
 
 object Constants {
-    const val BASE_API_PATH = "https://back-vynils-18a6153cd151.herokuapp.com/"
+    const val BASE_API_PATH = "https://9a43255d-aca2-4e23-a6d2-cbf103bed066.mock.pstmn.io/"
 }
