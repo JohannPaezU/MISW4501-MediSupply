3eae3e366321386e8d4cece2619ab43ff22be9a6
Miguel Fernando Padilla Espino <m.padillae@uniandes.edu.co>
2025-10-11 02:05:16 -0500

    mobile: add PrefsManager for user authentication data handling and update logout functionality

--- mobile/app/src/main/java/com/mfpe/medisupply/ui/MainActivity.kt
+++ mobile/app/src/main/java/com/mfpe/medisupply/ui/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.mfpe.medisupply.R
 import com.mfpe.medisupply.databinding.ActivityMainBinding
+import com.mfpe.medisupply.utils.PrefsManager
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,11 +25,10 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)
 
-        // Manejar clic en Cerrar sesión
         navView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_logout -> {
-                    // Redirigir a LoginActivity
+                    PrefsManager.getInstance(this).clear()
                     val intent = Intent(this, LoginActivity::class.java)
                     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                     startActivity(intent)
@@ -46,7 +46,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        // Manejar clic en el FAB
         binding.fabAdd.setOnClickListener {
             // Aquí puedes agregar la funcionalidad que desees
             // Por ejemplo, navegar a una pantalla de agregar producto
