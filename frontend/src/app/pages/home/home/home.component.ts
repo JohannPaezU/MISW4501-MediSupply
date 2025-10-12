39d9138ef3639804e026f829f663a29269261a22
Juan Sebastian Cervantes Restrepo <js.cervantes@uniandes.edu.co>
2025-10-12 18:59:00 -0400

    plantilla inicial del frontend

---
+++ frontend/src/app/pages/home/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  userName = 'Juan Sebastian';
+}
