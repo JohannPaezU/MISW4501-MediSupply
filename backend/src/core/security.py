07dc9c1c4cc14060467112050ff564e4fc662e4e
JohannPaezU <js.paezc1@uniandes.edu.co>
2025-10-15 01:29:58 -0500

    Login and verify otp development

--- backend/src/core/security.py
+++ backend/src/core/security.py
@@ -1,4 +1,10 @@
+from datetime import timezone, datetime, timedelta
+from typing import Any
+
 import bcrypt
+import jwt
+
+from src.core.config import settings
 
 
 def hash_password(password: str) -> str:
@@ -10,7 +16,16 @@ def hash_password(password: str) -> str:
 
 def verify_password(
     plain_password: str, hashed_password: str
-) -> bool:  # pragma: no cover
+) -> bool:
     return bcrypt.checkpw(
         plain_password.encode("utf-8"), hashed_password.encode("utf-8")
     )
+
+
+def create_access_token(data: dict[str, Any]) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.jwt_secret_key, algorithm=settings.jwt_algorithm)
+
+    return encoded_jwt
--- backend/tests/containers/postgres_test_container.py
+++ backend/tests/containers/postgres_test_container.py
@@ -1,6 +1,7 @@
 import os
 from testcontainers.postgres import PostgresContainer
 
+from src.core.config import settings
 from src.core.logging_config import logger
 
 
@@ -9,10 +10,10 @@ class PostgresTestContainer:
 
     def __init__(self):
         self.postgres_container = PostgresContainer(
-            username=os.environ["POSTGRES_USER"],
-            password=os.environ["POSTGRES_PASSWORD"],
-            dbname=os.environ["POSTGRES_DB"],
-        ).with_bind_ports(5432, os.environ["POSTGRES_PORT"])
+            username=settings.postgres_user,
+            password=settings.postgres_password,
+            dbname=settings.postgres_db,
+        ).with_bind_ports(5432, settings.postgres_port)
 
     def start(self) -> None:
         logger.info("Starting Postgres test container...")
