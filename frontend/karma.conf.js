3664457f562d52c74c7d691693716c61c3b8aabb
Juan Sebastian Cervantes Restrepo <js.cervantes@uniandes.edu.co>
2025-10-15 20:52:41 -0500

    se agregan test de login y sidebar, pendientes los del core

---
+++ frontend/karma.conf.js
@@ -0,0 +1,35 @@
+// Karma configuration file
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // Opciones de Jasmine
+      },
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true, // elimina trazas duplicadas
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/frontend'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ],
+      includeAllSources: true,
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    restartOnFileChange: true
+  });
+};
