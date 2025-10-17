c14d1f74ac6f4aecce4bf100d83ad270e353ccae
JohannPaezU <js.paezc1@uniandes.edu.co>
2025-10-17 13:23:58 -0500

    Role-Based Security Access Development

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,6 +6,8 @@ from src.db.database import init_database
 from src.errors.exception_handlers import setup_exception_handlers
 from src.routers.auth_router import auth_router
 from src.routers.health_check_router import health_check_router
+from src.routers.seller_router import seller_router
+from src.routers.zone_router import zone_router
 
 version = "1.0"
 prefix = f"/api/v{version.split('.')[0]}"
@@ -13,6 +15,8 @@ app = FastAPI(title="MediSupply API", version=version)
 
 app.include_router(health_check_router, prefix=prefix)
 app.include_router(auth_router, prefix=prefix)
+app.include_router(zone_router, prefix=prefix)
+app.include_router(seller_router, prefix=prefix)
 setup_exception_handlers(app)
 
 
