46cd64ad462df5b5600c29380b375d738c38b1e0
Juan Sebastian Cervantes Restrepo <js.cervantes@uniandes.edu.co>
2025-10-17 18:35:54 -0500

    se agregan los test de proveedores

--- frontend/src/app/interfaces/proveedor.intrface.ts
+++ frontend/src/app/interfaces/proveedor.intrface.ts
@@ -1,7 +1,3 @@
-/**
- * Interfaz base que representa la estructura de un proveedor recibido desde el backend.
- * Coincide con el schema `ProviderBase` del backend.
- */
 export interface ProviderBase {
   id: string;
   name: string;
@@ -11,23 +7,14 @@ export interface ProviderBase {
   image_url: string | null;
   email: string;
   phone: string;
-  created_at: string; // ISO date string
+  created_at: string;
 }
 
-/**
- * Interfaz para la respuesta del endpoint que obtiene todos los proveedores.
- * Coincide con el schema `GetProvidersResponse` del backend.
- */
 export interface GetProvidersResponse {
   total_count: number;
   providers: ProviderBase[];
 }
 
-
-/**
- * Interfaz que representa la estructura de un proveedor para la solicitud de creación.
- * Coincide con el schema `ProviderCreateRequest` del backend.
- */
 export interface ProviderCreateRequest {
   name: string;
   rit: string;
@@ -38,9 +25,5 @@ export interface ProviderCreateRequest {
   phone: string;
 }
 
-/**
- * Interfaz que representa la respuesta al crear un proveedor.
- * Hereda de ProviderBase ya que la respuesta contiene todos los campos.
- */
 export interface ProviderCreateResponse extends ProviderBase {}
 
